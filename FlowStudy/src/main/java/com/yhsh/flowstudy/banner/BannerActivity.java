06fbc8e458b489cd8a3a8c3d64177ba645c86636
xiayiye5 <13343401268@qq.com>
2025-06-14 11:43:36 +0800

    add 添加小圆点滑动逻辑

--- FlowStudy/src/main/java/com/yhsh/flowstudy/banner/BannerActivity.java
+++ FlowStudy/src/main/java/com/yhsh/flowstudy/banner/BannerActivity.java
@@ -1,8 +1,11 @@
 package com.yhsh.flowstudy.banner;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -13,8 +16,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BannerActivity extends AppCompatActivity {
+    private static final String TAG = "BannerActivity";
     int[] imgList = new int[]{R.drawable.one, R.drawable.two, R.drawable.three};
     List<ImageView> listImageView = new ArrayList<>();
+    private int pointLeftMargin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class BannerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_banner);
         ViewPager vp = findViewById(R.id.view_pager);
         LinearLayout pointGroup = findViewById(R.id.point_group);
+        ImageView pointSelect = findViewById(R.id.point_select);
         for (int i = 0; i < imgList.length; i++) {
             ImageView imageView = new ImageView(getApplicationContext());
             imageView.setBackgroundResource(imgList[i]);
@@ -38,10 +44,30 @@ public class BannerActivity extends AppCompatActivity {
             pointGroup.addView(point);
         }
         vp.setAdapter(new BannerAdapter(listImageView));
+        pointSelect.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //回调此方法后布局才绘制完成，此时方可拿到当前页面的大小、间距等信息
+                //获取滑动一页小圆点需要滑动的距离，第二个小圆点的左边距减去第一个小圆点的左边距
+                if (pointGroup.getChildCount() > 1) {
+                    pointLeftMargin = pointGroup.getChildAt(1).getLeft() - pointGroup.getChildAt(0).getLeft();
+                }
+                //会执行多次回调需要第一次收到后进行移除
+                pointSelect.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
         vp.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+                Log.d(TAG, "onPageScrolled position:" + position + "滑动的百分比:" + positionOffset);
+                //滑动百分比乘以滑动距离
+                int margin = (int) ((positionOffset + position) * pointLeftMargin);
+                Log.d(TAG, "onPageScrolled margin:" + margin);
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) pointSelect.getLayoutParams();
+                //根据当前滑动百分比设置小圆点的位置
+                layoutParams.leftMargin = margin;
+                pointSelect.setLayoutParams(layoutParams);
             }
 
             @Override
