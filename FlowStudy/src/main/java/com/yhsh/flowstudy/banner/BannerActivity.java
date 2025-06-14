d1f9dab9068919bb3058b7f4e548aae91aee83a1
xiayiye5 <13343401268@qq.com>
2025-06-14 11:30:49 +0800

    add 添加小圆点

--- FlowStudy/src/main/java/com/yhsh/flowstudy/banner/BannerActivity.java
+++ FlowStudy/src/main/java/com/yhsh/flowstudy/banner/BannerActivity.java
@@ -2,6 +2,7 @@ package com.yhsh.flowstudy.banner;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -20,11 +21,33 @@ public class BannerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banner);
         ViewPager vp = findViewById(R.id.view_pager);
+        LinearLayout pointGroup = findViewById(R.id.point_group);
         for (int i = 0; i < imgList.length; i++) {
             ImageView imageView = new ImageView(getApplicationContext());
             imageView.setBackgroundResource(imgList[i]);
             listImageView.add(imageView);
+            //添加3个点
+            ImageView point = new ImageView(getApplicationContext());
+            point.setImageResource(R.drawable.shpe_point_normal);
+            //从第二个小店开始设置小点之间的间距
+            if (i > 0) {
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                params.leftMargin = 10;
+                point.setLayoutParams(params);
+            }
+            pointGroup.addView(point);
         }
         vp.setAdapter(new BannerAdapter(listImageView));
+        vp.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+        });
     }
 }
