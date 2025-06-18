9c51dab066eb8e2a22eebef800798e2e848f47be
xiayiye5 <13343401268@qq.com>
2025-06-18 14:13:27 +0800

    add 添加26个字母的点击事件

--- FlowStudy/src/main/java/com/yhsh/flowstudy/QuickSearchActivity.java
+++ FlowStudy/src/main/java/com/yhsh/flowstudy/QuickSearchActivity.java
@@ -3,6 +3,9 @@ package com.yhsh.flowstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.yhsh.flowstudy.view.QuickIndexBar;
 
 public class QuickSearchActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class QuickSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_search);
+        QuickIndexBar quickIndexBar = findViewById(R.id.quick_index_bar);
+        quickIndexBar.setOnLetterChangeListener(letter -> {
+            Toast.makeText(getApplicationContext(), letter, Toast.LENGTH_SHORT).show();
+        });
     }
 }
--- FlowStudy/src/main/java/com/yhsh/flowstudy/view/QuickIndexBar.java
+++ FlowStudy/src/main/java/com/yhsh/flowstudy/view/QuickIndexBar.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -18,9 +20,12 @@ public class QuickIndexBar extends View {
     private Paint mPaint;
     private int layoutHeight;
     private int layoutWidth;
+    //记录上次点击的字母
+    int lastIndex = -1;
     private static final String TAG = "QuickIndexBar";
     //26个英文字母集合
     private final String[] alphabetData = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private float cellHeight;
 
 
     public QuickIndexBar(Context context) {
@@ -64,7 +69,7 @@ public class QuickIndexBar extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        float cellHeight = layoutHeight * 1.0f / alphabetData.length;
+        cellHeight = layoutHeight * 1.0f / alphabetData.length;
         float xPosition = layoutWidth * 1.0f / 2;
         for (int i = 0; i < alphabetData.length; i++) {
             //Y坐标的位置是26个格子每个格子的高度一半+文字高度的一半
@@ -78,5 +83,43 @@ public class QuickIndexBar extends View {
         Rect mRect = new Rect();
         mPaint.getTextBounds(text, 0, text.length(), mRect);
         return mRect.height();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            //按下和移动的时候都需要回调点击的字母，只需要判断当前的Y坐标除以每个格子的高度就是当前的字母索引
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                float y = event.getY();
+                int index = (int) (y / cellHeight);
+                if (lastIndex != index) {
+                    if (listener != null && index < alphabetData.length) {
+                        listener.onLetterChange(alphabetData[index]);
+                    }
+                    Log.d(TAG, "当前选中的文字: " + alphabetData[index]);
+                    lastIndex = index;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                //手指抬起时候需要重置
+                lastIndex = -1;
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    //添加字母的点击事件回调
+    public interface OnLetterChangeListener {
+        void onLetterChange(String letter);
+    }
+
+    OnLetterChangeListener listener;
+
+    public void setOnLetterChangeListener(OnLetterChangeListener listener) {
+        this.listener = listener;
     }
 }
